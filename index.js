320d1598a1697559ff731fe073b56f73778ab882
Brittany Morris <brittmmendez@gmail.com>
2018-03-15 12:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
+//returns an driver with the orignal key value pairs and the new key value pair ‣
+//it does not modify the original driver, but rather returns a clone with the new data
   const newDriver= {...driver};
   newDriver[key]=value;
   return newDriver;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+//updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver
   driver[key]=value;
   return driver;
 }
